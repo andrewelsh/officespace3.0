0a797be22e103efd81023b8bcd69a4b717a263b5
Andre Welsh <andre.welsh.dev@gmail.com>
2022-09-21 06:35:44 -0400

    updated error redirect url

--- src/Error.js
+++ src/Error.js
@@ -1,35 +1,30 @@
-import React, { useEffect } from 'react'
-import Footer from './components/Footer';
+import React, { useEffect } from "react";
+import Footer from "./components/Footer";
 import Nav from "react-bootstrap/Nav";
 
-
 const Error = () => {
-  
-    useEffect(() => {
-  
-        setTimeout(function () {
-          window.location.href = '/';
-        }, 1500);
-      });
-    return (
+  useEffect(() => {
+    setTimeout(function () {
+      window.location.href = "https://andrewelsh.github.io/officespace3.0/";
+    }, 1500);
+  });
+  return (
     <div>
-    <Nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-    
-      Office Space
+      <Nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        Office Space
       </Nav>
-    <br />
-    <br />
-    <br />
-    <center>
-    <h1>You've made it here somehow?</h1>
-    <hr />
-    <h4>Sending you somewhere more helpful</h4>
-    <hr />
-
-    </center>
-<Footer />
+      <br />
+      <br />
+      <br />
+      <center>
+        <h1>You've made it here somehow?</h1>
+        <hr />
+        <h4>Sending you somewhere more helpful</h4>
+        <hr />
+      </center>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
