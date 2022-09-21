42ed46c5e128badf771000abffcce1532e41acfa
Andre Welsh <andre.welsh.dev@gmail.com>
2022-09-20 22:55:17 -0400

    updated logged out link

--- src/components/LoggedOut.js
+++ src/components/LoggedOut.js
@@ -1,42 +1,31 @@
-import React, { useEffect } from 'react'
-import Footer from './Footer';
+import React, { useEffect } from "react";
+import Footer from "./Footer";
 import Nav from "react-bootstrap/Nav";
 
-
-
 const LoggedOut = () => {
-
   useEffect(() => {
-  
     setTimeout(function () {
-      window.location.href = '/';
+      window.location.href = "https://andrewelsh.github.io/officespace3.0/";
     }, 1500);
   });
   return (
     <>
-    <Nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-    
-    Office Space
-    </Nav>
-    <center>
-    <br />
-    <br />
-    <br />
-    <div><h3>
-    
-    Come Back Again Soon 
-    </h3>
-    <hr />
-    <p>
-    
-    You are now Logged Out
-    
-    </p>
-    </div>
-    </center>
-    <Footer />
+      <Nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        Office Space
+      </Nav>
+      <center>
+        <br />
+        <br />
+        <br />
+        <div>
+          <h3>Come Back Again Soon</h3>
+          <hr />
+          <p>You are now Logged Out</p>
+        </div>
+      </center>
+      <Footer />
     </>
-    )
-  }
+  );
+};
 
-export default LoggedOut
+export default LoggedOut;
